3a1849446bf0d676fa484dea9c9ad9d8e64d42cc
Nebojsa Markovic <nemarkovic@yandex.com>
2021-01-03 02:39:43 +0100

    Iss_1 : Adds tcl script for generating compartor project

--- src/CMPR/cmpr.vhd
+++ src/CMPR/cmpr.vhd
@@ -4,8 +4,22 @@
 -- Author      : Nebojsa Markovic
 -- Mail        : nemarkovic@yandex.com
 -- Date        : 09 December 2020
--- Description :
---
+-- Description : The comparator finds the biggest number!
+--               The design is intended to be used while developing
+--               alghorithms which would need such a design block, but it
+--               still it is not deacided how much operands there is on the
+--               input of the block and what is thers size. 
+--               The design is parametrised and size of the input operand
+--               and nuber of the operands on the input.
+--               When the final parameter numbers are deacided, it is possible
+--               to implementdesign in other ways in order to save resources
+--       Input - It is expected for all operands to be concatenated in one
+--               input vector, i_vec
+--      Output - Output - G_NO_OF_INPUTS'size vector. Bits on the position
+--               of the biggest number from input vector is set to one.
+--               Other bits are set to zero
+--  Parameters - G_NO_OF_INPUTS - number of operands
+--               G_VEC_SIZE     - Operand size in bits
 -------------------------------------------------------------------------
 -------------------------------------------------------------------------
 library ieee;
@@ -15,40 +29,55 @@ library ieee;
 
 entity cmpr is
     generic(
-        G_NO_OF_INPUTS : natural := 8;
-        G_VEC_SIZE     : natural := 16 );
+        -- Number of input operands
+        G_NO_OF_INPUTS : natural :=  8;
+        -- Size of one operand
+        G_VEC_SIZE     : natural := 32);
     port (
+        -- Input vector - input operands concatenated in one vector
         i_vec          : in  std_logic_vector (G_NO_OF_INPUTS * G_VEC_SIZE -1 downto 0);
+        -- Output vector - set to '1' on the possition of the biggest number in input vector
         o_vec          : out std_logic_vector (G_NO_OF_INPUTS -1 downto 0));
 end cmpr;
 
 architecture RTL of cmpr is
 
    -- MAT type is array of inputs for the OR circuits, which outputs are used as MUX select
-   type TYPE_or_MTRX is array (G_VEC_SIZE -1 downto 0 ) of std_logic_vector(G_NO_OF_INPUTS -1 downto 0);
+   type TYPE_or_MTRX is array (G_VEC_SIZE -1 downto 0) of std_logic_vector(G_NO_OF_INPUTS -1 downto 0);
+
+   -- Input matrix for the OR circuits in the deign
    signal s_iOR_vec  : TYPE_or_MTRX; -- := (others =>  ( others => '0'));
+   -- Output vector with results from OR circuits
    signal s_oOR_vec  : std_logic_vector (G_VEC_SIZE -1 downto 0);
 
-   type TYPE_iMUX_MTRX is array (G_VEC_SIZE -1 downto 0 ) of std_logic_vector(G_NO_OF_INPUTS *2 -1 downto 0);
+   -- Input MUX matrix
+   type TYPE_iMUX_MTRX is array (G_VEC_SIZE -1 downto 0) of std_logic_vector(G_NO_OF_INPUTS *2 -1 downto 0);
    signal s_iMUX_vec  : TYPE_iMUX_MTRX; -- := (others =>  ( others => '0'));
 --   signal s_iMUX_sel : std_logic_vector (f_clog2(G_NO_OF_INPUTS) -1 downto 0);
 
-   type TYPE_oMUX_MTRX is array (G_VEC_SIZE -1 downto 0 ) of std_logic_vector(G_NO_OF_INPUTS -1 downto 0);
+   -- Output MUX matrix
+   type TYPE_oMUX_MTRX is array (G_VEC_SIZE -1 downto 0) of std_logic_vector(G_NO_OF_INPUTS -1 downto 0);
    signal s_oMUX_vec  : TYPE_oMUX_MTRX; -- := (others =>  ( others => '0'));
 
-   type TYPE_iAND_MTRX is array (G_VEC_SIZE -1 -1 downto 0 ) of std_logic_vector(2 * G_NO_OF_INPUTS -1 downto 0);
+   -- Input MUX matrix for the AND circuits in the deign
+   type TYPE_iAND_MTRX is array (G_VEC_SIZE -1 -1 downto 0) of std_logic_vector(2 * G_NO_OF_INPUTS -1 downto 0);
    signal s_iAND_vec : TYPE_iAND_MTRX;
 
-   type TYPE_oAND_MTRX is array (G_VEC_SIZE -1 downto 0 ) of std_logic_vector(G_NO_OF_INPUTS -1 downto 0);
+   -- Output MUX matrix for the AND circuits in the deign
+   type TYPE_oAND_MTRX is array (G_VEC_SIZE -1 downto 0) of std_logic_vector(G_NO_OF_INPUTS -1 downto 0);
    signal s_oAND_vec : TYPE_oAND_MTRX;
 begin
-
-
 
 genX : for i in G_VEC_SIZE -1 downto 0 generate
 
    s_iOR_vec(i)  <= s_oAND_vec(i);
 
+   -- OR circuit in the design is G_NO_OF_INPUTS size OR circuit
+   -- that is used to check if any of the input operands, on the
+   -- "i-th" possition has a '1' (this '1' is important only if the
+   -- operand is still a candidate for the biggest number. AND cicuit
+   -- in combination with MUXes is used to propagate ones for each 
+   -- candidate for biggest num)
    logic_inst : logic
       generic map (
          G_NO_OF_INPUTS => G_NO_OF_INPUTS,
